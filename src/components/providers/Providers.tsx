6f6ae95594e5afba802138a59e11f6ab082c1d16
Zishan Mohd <zishan2539@gmail.com>
2023-12-09 22:34:56 +0530

    Fix: login logic updated

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -2,11 +2,7 @@ import { ThemeProvider } from 'src/store/themeContext'
 import { AccountAbstractionProvider } from 'src/store/accountAbstractionContext'
 
 const Providers = ({ children }: { children: JSX.Element }) => {
-  return (
-    <ThemeProvider>
-      <AccountAbstractionProvider>{children}</AccountAbstractionProvider>
-    </ThemeProvider>
-  )
+  return <AccountAbstractionProvider>{children}</AccountAbstractionProvider>
 }
 
 export default Providers
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import Appc from './App1'
 import './index.css'
+import Providers from './components/providers/Providers'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-root.render(<Appc />)
+root.render(
+  <Providers>
+    <Appc />
+  </Providers>
+)
